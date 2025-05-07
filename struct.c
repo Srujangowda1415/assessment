0e28fabbef0493d4aa1cad661a0c3b3bd72c94f6
srujan <srujan.gowda@s.amity.edu>
2025-05-07 12:03:13 +0530

    third commit

--- struct.c
+++ struct.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int temp(float a) {
-    printf("The temp is: %f\n", (a*9/5) + 32);
+    printf("The temp is: %f\n", (b*9/5) + 72);
 
-    return a*9/5 + 32;
+    return b*9/5 + 72;
     }
+
 int main() {
     temp(23.9);
     return 0;
